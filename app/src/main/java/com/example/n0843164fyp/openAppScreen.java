1d6f7b3013764f10996e2b392858eb5e25b141ed
n08tommyn <N0843164@my.ntu.ac.uk>
2024-02-28 15:13:01 +0000

    Testing Git

---
+++ app/src/main/java/com/example/n0843164fyp/openAppScreen.java
@@ -0,0 +1,14 @@
+package com.example.n0843164fyp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class openAppScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_app_screen);
+    }
+}
